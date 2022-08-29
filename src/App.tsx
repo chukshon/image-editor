a6a41d49381809b85eb75e094b8442eab7d9d4db
ChukwukaHonour <ChukwukaHonour@gmail.com>
2022-08-29 15:06:51 +0100

    html markup started

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
-import { Wrapper } from './style'
+import { Wrapper, ContainerStyled } from './style'
 
 function App() {
+  const [value, setValue] = useState('')
+  const handleSlideRange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }
   return (
     <Wrapper>
-      <h1>Hello</h1>
+      <ContainerStyled>
+        <h1>Easy Image Editor</h1>
+        <div className='editing_panel'>
+          <div className='filters'>
+            <div className='filter_btns'>
+              <h3>Filters</h3>
+              <button>Brightness</button>
+              <button>Saturation</button>
+              <button>Inversion</button>
+              <button>GreyScale</button>
+            </div>
+
+            <div className='slide_range'>
+              <div className='top_heading'>
+                <span>Brighness</span>
+                <span>{value}%</span>
+              </div>
+              <input
+                type='range'
+                min='0'
+                max='200'
+                value={value}
+                className='slider'
+                id='myRange'
+                onChange={handleSlideRange}
+              ></input>
+            </div>
+          </div>
+          <div className='image_preview'></div>
+        </div>
+        <div className='action_btns'></div>
+      </ContainerStyled>
     </Wrapper>
   )
 }
--- src/style.ts
+++ src/style.ts
@@ -1,3 +1,15 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`
+
+export const ContainerStyled = styled.div`
+  width: 500px;
+  height: 500px;
+  background-color: var(--white);
+  border-radius: var(--radius-10);
+`
